8f4589ec063a85525184450630ca8840a0bdd384
ervisaaa <ervisa.cakaj@gmail.com>
2020-09-28 00:20:54 +0200

    1

---
+++ EFDbFirstApproachExample/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDbFirstApproachExample.Models;
+
+namespace EFDbFirstApproachExample.Areas.Admin.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
